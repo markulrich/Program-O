be3cc40fad4da110ee4c887a04d6401849aca2e8
Mark Ulrich <mulrich@stanford.edu>
2014-12-02 23:47:51 -0800

    MOAR FEATURES!

--- config/get_quote.py
+++ config/get_quote.py
@@ -1,4 +1,4 @@
-import json, os, random, sys
+import json, os, random, sys, string
 
 class Features:
     def __init__(self, human_input, quotes):
@@ -10,14 +10,68 @@ class Features:
         words = sentance.split()
         return sum(len(w) for w in words) / len(words)
 
+    @staticmethod
+    def similarity_word_feature(userInput,quote):
+        userInput = userInput.lower()
+        quote = quote.lower()
+        stripped_input = string.split("".join(c for c in userInput if c not in ('!','.',':','?',',')))
+        stripped_quote = string.split("".join(c for c in quote if c not in ('!','.',':','?',',')))
+        num_same_words = len(set(stripped_input).intersection(set(stripped_quote)))
+        num_total_words = len(set(stripped_input+stripped_quote))
+        return float(num_same_words)/num_total_words
+
+    # returns 1 if both userInput and quote are questions, 0 otherwise
+    @staticmethod
+    def question_feature(userInput,quote):
+        if (userInput[len(userInput)-1] == '?' and quote[len(quote)-1]=='?'): return 1
+        else: return 0
+
+
+
+    # returns the proportion of different matching ngrams in userInput and quote in relation to the total number of different ngrams in both
+    @staticmethod
+    def similarity_ngram_feature(userInput,quote):
+        n = 3 #defines range of n_gram
+        userInput = userInput.lower()
+        quote = quote.lower()
+        stripped_input = "".join(c for c in userInput if c not in ('!','.',':','?',','," "))
+        stripped_quote = "".join(c for c in quote if c not in ('!','.',':','?',','," "))
+        total_ngrams = set([])
+        same_ngrams = set([])
+        if (n<=len(stripped_input) and n<=len(stripped_quote)):
+            for i in range(len(stripped_input)-(n-1)):
+                for j in range(len(stripped_quote)-(n-1)):
+                    n_gram_input = stripped_input[i:(i+n)]
+                    n_gram_quote = stripped_quote[j:(j+n)]
+                    total_ngrams.add(n_gram_input)
+                    total_ngrams.add(n_gram_quote)
+                    if (n_gram_input == n_gram_quote):
+                        if n_gram_input not in same_ngrams:
+                            same_ngrams.add(n_gram_input)
+            return float(len(same_ngrams))/len(total_ngrams)
+
+        else: return 0
+
+    @staticmethod
+    def similar_word_lengths(i, q):
+        i_av = Features.avgWordLen(i)
+        q_av = Features.avgWordLen(q)
+        return abs(i_av - q_av) / max(i_av, q_av)
+
     FEATURES = [
-        lambda i, q: abs(2 * len(i) - len(q)),
-        lambda i, q: abs(Features.avgWordLen(i) - Features.avgWordLen(q))
+        lambda i, q: abs(2 * len(i) - len(q)) / max(2 * len(i), len(q)),
+        lambda i, q: Features.similar_word_lengths(i, q),
+        lambda i, q: Features.similarity_word_feature(i, q),
+        lambda i, q: Features.question_feature(i, q),
+        lambda i, q: Features.similarity_ngram_feature(i, q)
     ]
 
     WEIGHTS = [
         -5.0,
-        -2.0
+        -2.0,
+        10,
+        -30,
+        5
     ]
 
     def getFeats(self, quote):
